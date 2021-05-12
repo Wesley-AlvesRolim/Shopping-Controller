b43c804255e0b23c8c1661818c954cc9a8ec5b23
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-05-12 10:28:25 -0300

    function to controll data and to update balance in real-time

---
+++ public/scripts/openedForm.js
@@ -0,0 +1,40 @@
+import { form } from './form';
+import { utils } from './utils';
+
+export function openedForm(value, image) {
+    const cost = document.querySelector('.cost');
+    cost.innerHTML = `Custo: ${utils.formatPrice(0)}`;
+    createImg(image);
+    focus(value);
+}
+
+function createImg(imageLink) {
+    const divImage = document.querySelector('.divImage');
+    const img = document.createElement('img');
+    divImage.appendChild(img);
+
+    img.classList.add('img');
+    img.setAttribute('src', imageLink);
+}
+
+function focus(value, stock = 100) {
+    const cost = document.querySelector('.cost');
+    const quantity = document.querySelector('.quantity');
+
+    document.querySelector('input[type="number"]').addEventListener('focus', () => {
+        form.productsQuantity.oninput = function() {
+            if (form.catchValues().productsQuantity >= stock) Number(form.productsQuantity.value = stock);
+
+            const checkName = form.catchValues().productsQuantity > 1 ? ' Unidades' : ' Unidade';
+            const costValue = value * form.catchValues().productsQuantity;
+
+            if (form.catchValues().productsQuantity <= 0) {
+                cost.innerHTML = 'Custo: ' + utils.formatPrice(costValue);
+                quantity.innerHTML = 0 + checkName;
+                return;
+            }
+            cost.innerHTML = 'Custo: ' + utils.formatPrice(costValue);
+            quantity.innerHTML = form.catchValues().productsQuantity + checkName;
+        };
+    });
+}
