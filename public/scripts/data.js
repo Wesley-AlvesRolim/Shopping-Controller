58aaf9f4cfae198360768cdb2957d5f748bf3ac8
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-05-12 10:25:52 -0300

    eslint fix all errors

--- public/scripts/data.js
+++ public/scripts/data.js
@@ -1,76 +1,76 @@
 export const products = [{
         value: 87.3,
-        productName: "CyberPunk 2077 - PS4",
+        productName: 'CyberPunk 2077 - PS4',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/1466225/4/1466225463_1SZ.jpg',
     },
     {
         value: 439.99,
-        productName: "Fritadeira Sem Óleo Oster",
+        productName: 'Fritadeira Sem Óleo Oster',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/1481124/3/1481124311P1.jpg',
     },
     {
         value: 279.99,
-        productName: "Headset Gamer Havit H2015d",
+        productName: 'Headset Gamer Havit H2015d',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/1809794/0/1809794044_1SZ.jpg',
     },
     {
         value: 4699.99,
-        productName: "Lenovo Ideapad L340",
+        productName: 'Lenovo Ideapad L340',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/2430049/7/2430049779_1SZ.jpg',
     },
     {
         value: 239.99,
-        productName: "Liquidificador Philips",
+        productName: 'Liquidificador Philips',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/134532/8/134532811_4GG.jpg',
     },
     {
         value: 598.5,
-        productName: "Micro - ondas Consul",
+        productName: 'Micro-ondas Consul',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img7/01/00/item/116796/3/116796362_5GG.jpg',
     },
     {
         value: 1265.25,
-        productName: "Monitor LED 25 '' Gamer LG ",
+        productName: 'Monitor LED 25\'\' Gamer LG',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/1729775/6/1729775687_4GG.jpg',
     },
     {
         value: 1151.1,
-        productName: "Moto G9 Play",
+        productName: 'Moto G9 Play',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/1962217/0/1962217075_1SZ.jpg',
     },
     {
         value: 159.6,
-        productName: "Mouse Gamer Redragon Chroma",
+        productName: 'Mouse Gamer Redragon Chroma',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/46419/3/46419319_3GG.jpg',
     },
     {
         value: 2922.99,
-        productName: "Playstation 4 + 3 Games",
+        productName: 'Playstation 4 + 3 Games',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/2317562/0/2317562028P1.jpg',
     },
     {
         value: 10377.93,
-        productName: "Smart TV OLED 65 '' LG ",
+        productName: 'Smart TV OLED 65\'\' LG',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/1735043/1/1735043183_1SZ.jpg',
     },
     {
         value: 339,
-        productName: "Teclado Gamer HyperX Alloy Core",
+        productName: 'Teclado Gamer HyperX Alloy Core',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/98025/6/98025652_1SZ.jpg',
     },
     {
         value: 235.83,
-        productName: "The Last Of Us Part II - PS4",
+        productName: 'The Last Of Us Part II - PS4',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/1459449/0/1459449098_1SZ.jpg',
     },
     {
         value: 264.48,
-        productName: "SSD 240 gb Plus - Sandisk",
+        productName: 'SSD 240 gb Plus - Sandisk',
         image: 'https://images-submarino.b2w.io/produtos/01/00/images/134409/7/134409750P1.jpg',
     },
     {
         value: 3403.08,
-        productName: "Xbox Series S",
+        productName: 'Xbox Series S',
         image: 'https://images-submarino.b2w.io/produtos/01/00/img/2117190/5/2117190565P1.jpg',
     },
-]
+];
