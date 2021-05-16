21dc75d764061ca92d292633965d9d1d26673f2e
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-05-16 18:07:12 -0300

    Better way to open overlays and cleaner

--- public/scripts/overlay.js
+++ public/scripts/overlay.js
@@ -1,30 +1,19 @@
 import { form } from './form';
 
 export const overlay = {
-    open() {
-        document.querySelector('.form-overlay').classList.add('active');
-        form.clearFields();
-    },
-    close() {
-        document.querySelector('.form-overlay').classList.remove('active');
-    },
-};
-
-export const sideBar = {
-    sideBarElement: document.querySelector('.overlay-side-bar'),
+    form: document.querySelector('.form-overlay'),
+    cart: document.querySelector('.cart-overlay'),
+    sideBar: document.querySelector('.overlay-side-bar'),
     menu: document.querySelector('.header__menu'),
+    open(element) {
+        if (element === 'form') form.clearFields();
+        this[element].classList.add('active');
+    },
+    close(element) {
+        this[element].classList.remove('active');
+    },
     openClose() {
         this.menu.classList.toggle('active');
-        this.menu.classList.toggle('fixed');
-        this.sideBarElement.classList.toggle('active');
-    }
-};
-export const cart = {
-    cartOverlay: document.querySelector('.cart-overlay'),
-    open() {
-        this.cartOverlay.classList.add('active');
-    },
-    close() {
-        this.cartOverlay.classList.remove('active');
+        this.sideBar.classList.toggle('active');
     }
 };
