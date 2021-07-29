044b032d79d38967968947df72f1ac2dd9711e6f
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-07-29 10:29:07 -0300

    Function decrese stock created

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -1,13 +1,25 @@
+import { products } from './data';
+import { cards } from './cards';
+
 export const utils = {
-    formatPrice(value) {
-        const converted = value.toLocaleString('pt-BR', {
-            style: 'currency',
-            currency: 'BRL',
-        });
-        return converted;
-    },
+  formatPrice(value) {
+    const converted = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+    return converted;
+  },
 };
 
 export function stock(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
-};
+}
+
+export function decreaseStock(obj, productsQuantity) {
+  const position = products.indexOf(obj);
+  const productBuy = products[position];
+  const calcStock = productBuy.stock - productsQuantity;
+  if (calcStock < 0) return;
+  productBuy.stock = calcStock;
+  if (productBuy.stock <= 0) cards.stockZero(position);
+}
